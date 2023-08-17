ce2ece13239d0fb014efa8766ca6b9513126ef5b
MiI0sz <gajda.milosz@gmail.com>
2023-08-18 01:40:48 +0200

    basic design done

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import "./style.css";
 import { Carousel, initTE } from "tw-elements";
 import Masonry from 'masonry-layout';
 
-initTE({ Carousel });
+window.onload = () => {
+  initTE({ Carousel });
 
-const grid = document.getElementById('masonry-grid');
-const masonry = new Masonry(grid, {
-    columnWidth: '.masonry-sizer',
-    itemSelector: '.masonry-item',
-    gutter: 40
-  });
-
-console.log('main.js executed');
+  const grid = document.getElementById('masonry-grid');
+  const masonry = new Masonry(grid, {
+      columnWidth: '.masonry-sizer',
+      itemSelector: '.masonry-item',
+      gutter: 40
+    });
+  
+  console.log('main.js executed');
+}
 
 // https://webpack.js.org/guides/asset-modules/
